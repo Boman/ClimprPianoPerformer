5ec03f84a9d624fa157f3fa5f3774b33a71224d0
falko <falko@falko-Vostro1310>
2012-10-30 17:53:52 +0100

    minor changes

--- src/org/climpr/pianoperformer/gui/PianoKeyboard.java
+++ src/org/climpr/pianoperformer/gui/PianoKeyboard.java
@@ -12,11 +12,16 @@ package org.climpr.pianoperformer.gui;
  *  GNU General Public License for more details.
  */
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -57,19 +62,21 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 	private Bitmap bufferBitmap;
 	/** The bitmap for double-buffering */
 	private Canvas bufferCanvas;
-
 	/** The canvas for double-buffering */
+
+	private final int NUM_KEYS;
+	private final int NUM_WHITE_KEYS;
+	private final int KEY_NOTE_OFFSET;
+
+	private int[] keyPositions;
 
 	/*
 	 * Shade the given note with the given brush. We only draw notes from
 	 * notenumber 24 to 96. (Middle-C is 60).
 	 */
-	private final int NUM_KEYS;
-	private final int NUM_WHITE_KEYS;
-	private final int KEY_NOTE_OFFSET;
-
 	private int[] shadedKeys;
 
+	HashMap<Integer, PointF> touchPoints = new HashMap<Integer, PointF>();
 	private boolean[] touchedNotes;
 
 	/** Create a new Piano. */
@@ -83,13 +90,14 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 		gray2 = Color.rgb(90, 90, 90);
 		gray3 = Color.rgb(200, 200, 200);
 		shade = new int[2];
-		shade[0] = Color.rgb(210, 205, 220);
-		shade[1] = Color.rgb(150, 200, 220);
+		shade[0] = Color.rgb(0, 255, 0);
+		shade[1] = Color.rgb(0, 0, 255);
 
 		NUM_KEYS = 88;
 		NUM_WHITE_KEYS = 52;
 		KEY_NOTE_OFFSET = 21;
 
+		keyPositions = new int[NUM_KEYS];
 		shadedKeys = new int[NUM_KEYS];
 		touchedNotes = new boolean[NUM_KEYS];
 
@@ -115,6 +123,8 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 		BlackKeyWidth = WhiteKeyWidth / 2;
 		BlackKeyHeight = WhiteKeyHeight * 5 / 9;
 
+		calcKeyPositions();
+
 		int width = borderLR * 2 + WhiteKeyWidth * NUM_WHITE_KEYS;
 		int height = borderT + borderB + WhiteKeyHeight;
 		setMeasuredDimension(width, height);
@@ -122,6 +132,59 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 		bufferCanvas = new Canvas(bufferBitmap);
 		this.invalidate();
 		draw();
+	}
+
+	private void calcKeyPositions() {
+		int note = KEY_NOTE_OFFSET % 12; // zero means C
+		int pos = 0;
+		for (int i = 0; i < NUM_KEYS; i++) {
+			double posOffset = 0;//((octave - octaveStart) * 7 - noteStart + (noteStart > 4 ? (noteStart > 11 ? 2 : 1) : 0)) * WhiteKeyWidth;
+			int posAdd = 0;
+			switch (note) {
+			case 0:
+				posAdd = 1;
+				break;
+			case 1:
+				posOffset = -0.6f * BlackKeyWidth;
+				break;
+			case 2:
+				posAdd = 1;
+				break;
+			case 3:
+				posOffset = -0.4f * BlackKeyWidth;
+				break;
+			case 4:
+				posAdd = 1;
+				break;
+			case 5:
+				posAdd = 1;
+				break;
+			case 6:
+				posOffset = -0.6f * BlackKeyWidth;
+				break;
+			case 7:
+				posAdd = 1;
+				break;
+			case 8:
+				posOffset = -0.5f * BlackKeyWidth;
+				break;
+			case 9:
+				posAdd = 1;
+				break;
+			case 10:
+				posOffset = -0.4f * BlackKeyWidth;
+				break;
+			case 11:
+				posAdd = 1;
+				break;
+			}
+			keyPositions[i] = pos + (int) (posOffset);
+			pos += posAdd * WhiteKeyWidth;
+			note++;
+			if (note >= 12) {
+				note = 0;
+			}
+		}
 	}
 
 	@Override
@@ -157,32 +220,32 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 		bufferCanvas.drawRect(0, 0, 0 + WhiteKeyWidth * NUM_WHITE_KEYS, WhiteKeyHeight, paint);
 		paint.setStyle(Paint.Style.STROKE);
 
-		for (int i = 0; i < NUM_WHITE_KEYS; i++) {
-			int left = i * WhiteKeyWidth;
-			int midiNote = keyToMidiNumber(i * 2);
-			// fill the key
-			if (shadedKeys[midiNote - KEY_NOTE_OFFSET] != 0) {
-				paint.setStyle(Paint.Style.FILL);
-				paint.setColor(shade[0]);
-				bufferCanvas.drawRect(left, 0, left + WhiteKeyWidth, WhiteKeyHeight, paint);
-				paint.setStyle(Paint.Style.STROKE);
-			}
-			// draw white key separation
-			if (i > 0) {
-				paint.setColor(gray1);
-				bufferCanvas.drawLine(left, 0, left, WhiteKeyHeight, paint);
-				paint.setColor(gray2);
-				bufferCanvas.drawLine(left - 1, 1, left - 1, WhiteKeyHeight, paint);
-				paint.setColor(gray3);
-				bufferCanvas.drawLine(left + 1, 1, left + 1, WhiteKeyHeight, paint);
+		for (int i = 0; i < NUM_KEYS; i++) {
+			if (keyPositions[i] % WhiteKeyWidth == 0) {
+				int x1 = keyPositions[i];
+				// fill the key
+				if (shadedKeys[i] != 0) {
+					paint.setStyle(Paint.Style.FILL);
+					paint.setColor(shade[0]);
+					bufferCanvas.drawRect(x1, 0, x1 + WhiteKeyWidth, WhiteKeyHeight, paint);
+					paint.setStyle(Paint.Style.STROKE);
+				}
+				// draw white key separation
+				if (i > 0) {
+					paint.setColor(gray1);
+					bufferCanvas.drawLine(x1, 0, x1, WhiteKeyHeight, paint);
+					paint.setColor(gray2);
+					bufferCanvas.drawLine(x1 - 1, 1, x1 - 1, WhiteKeyHeight, paint);
+					paint.setColor(gray3);
+					bufferCanvas.drawLine(x1 + 1, 1, x1 + 1, WhiteKeyHeight, paint);
+				}
 			}
 		}
 
-		for (int i = 0; i < NUM_WHITE_KEYS; i++) {
-			// draw black keys
-			int midiNote = keyToMidiNumber(i * 2 + 1);
-			if (midiNote != -1 && midiNote - KEY_NOTE_OFFSET < NUM_KEYS) {
-				int x1 = (int) ((blackKeyOffsets(midiNote) + i + 1) * WhiteKeyWidth - 0.5 * BlackKeyWidth);
+		// draw black keys
+		for (int i = 0; i < NUM_KEYS; i++) {
+			if (keyPositions[i] % WhiteKeyWidth != 0) {
+				int x1 = keyPositions[i];
 				int x2 = x1 + BlackKeyWidth;
 
 				// draw the three sides
@@ -200,7 +263,7 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 				bufferCanvas.drawLine(x1 - 2, BlackKeyHeight + 2, x2 + 2, BlackKeyHeight + 2, paint);
 
 				// fill key
-				if (shadedKeys[midiNote - KEY_NOTE_OFFSET] == 0) {
+				if (shadedKeys[i] == 0) {
 					paint.setStyle(Paint.Style.FILL);
 					paint.setColor(gray1);
 					bufferCanvas.drawRect(x1, 0, x2, BlackKeyHeight, paint);
@@ -220,12 +283,20 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 
 		int PianoWidth = WhiteKeyWidth * NUM_WHITE_KEYS;
 		paint.setStyle(Paint.Style.FILL);
+
+		// draw the four borders
+		paint.setColor(gray1);
+		bufferCanvas.drawRect(0, 0, PianoWidth + borderLR * 2, borderT - 2, paint);
+		bufferCanvas.drawRect(0, 0, borderLR, borderT + WhiteKeyHeight + borderB, paint);
+		bufferCanvas.drawRect(0, borderT + WhiteKeyHeight, borderLR * 2 + PianoWidth, borderT + WhiteKeyHeight + borderB, paint);
+		bufferCanvas.drawRect(borderLR + PianoWidth, 0, PianoWidth + borderLR * 2, borderT + WhiteKeyHeight + borderB, paint);
+
 		paint.setColor(gray2);
 		bufferCanvas.drawLine(borderLR, borderT - 1, borderLR + PianoWidth, borderT - 1, paint);
 
 		bufferCanvas.translate(borderLR, borderT);
 
-		// Draw the gray bottoms of the white keys
+		// draw the gray bottoms of the white keys
 		for (int i = 0; i < NUM_WHITE_KEYS; i++) {
 			bufferCanvas.drawRect(i * WhiteKeyWidth + 1, WhiteKeyHeight + 2, i * WhiteKeyWidth + 1 + WhiteKeyWidth - 2, WhiteKeyHeight + 2 + borderLR / 2, paint);
 		}
@@ -233,36 +304,6 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 		bufferCanvas.translate(-borderLR, -borderT);
 
 		canvas.drawBitmap(bufferBitmap, 0, 0, paint);
-	}
-
-	private int keyToMidiNumber(int keyNumber) {
-		// TODO: fix for other values of KEY_NOTE_OFFSET than 21
-		int ret = (keyNumber / 14) * 12 + keyNumber % 14;
-		if (keyNumber % 14 == 3) {
-			return -1;
-		} else if (keyNumber % 14 > 3) {
-			ret--;
-		}
-		if (keyNumber % 14 == 9) {
-			return -1;
-		} else if (keyNumber % 14 > 9) {
-			ret--;
-		}
-		return ret + KEY_NOTE_OFFSET;
-	}
-
-	private float blackKeyOffsets(int midiNote) {
-		switch (midiNote % 12) {
-		case 3:
-		case 10:
-			return 0.08f;
-		case 1:
-		case 6:
-			return -0.08f;
-		case 8:
-			return 0;
-		}
-		return 0;
 	}
 
 	/**
@@ -277,7 +318,27 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 	}
 
 	private int getNoteForPixel(float x, float y) {
-		return (int) (x / WhiteKeyWidth) + KEY_NOTE_OFFSET;
+		x -= borderLR;
+		y -= borderT;
+		if (y > 0 && y < BlackKeyHeight) {
+			for (int i = 0; i < NUM_KEYS; i++) {
+				if (keyPositions[i] % WhiteKeyWidth != 0) {
+					if (x > keyPositions[i] && x < keyPositions[i] + BlackKeyWidth) {
+						return i + KEY_NOTE_OFFSET;
+					}
+				}
+			}
+		}
+		if (y > 0 && y < WhiteKeyHeight) {
+			for (int i = 0; i < NUM_KEYS; i++) {
+				if (keyPositions[i] % WhiteKeyWidth == 0) {
+					if (x > keyPositions[i] && x < keyPositions[i] + WhiteKeyWidth) {
+						return i + KEY_NOTE_OFFSET;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 
 	/** TODO ?? */
@@ -299,25 +360,73 @@ public class PianoKeyboard extends SurfaceView implements SurfaceHolder.Callback
 	/** When the Piano is touched, pause the midi player */
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		boolean[] newTouchedNotes = new boolean[NUM_KEYS];
-		switch (event.getAction()) {
+		int action = event.getActionMasked();
+
+		int index, id;
+		switch (action) {
 		case MotionEvent.ACTION_DOWN:
-		case MotionEvent.ACTION_MOVE:
 		case MotionEvent.ACTION_POINTER_DOWN:
-			int touchedNote = getNoteForPixel(event.getX(), event.getY()) - KEY_NOTE_OFFSET;
-			if (!touchedNotes[touchedNote]) {
-				// TODO
-				shadeKey(touchedNote + KEY_NOTE_OFFSET, 1);
+			index = event.getActionIndex();
+			id = event.getPointerId(index);
+			touchPoints.put(id, new PointF(event.getX(index), event.getY(index)));
+			break;
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_POINTER_UP:
+			index = event.getActionIndex();
+			id = event.getPointerId(index);
+			touchPoints.remove(id);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int length = event.getPointerCount();
+			for (int i = 0; i < length; i++) {
+				id = event.getPointerId(i);
+				try {
+					touchPoints.get(id).set(event.getX(i), event.getY(i));
+				} catch (IndexOutOfBoundsException e) {
+				}
 			}
-			newTouchedNotes[touchedNote] = true;
+			break;
+		}
+
+		boolean[] newTouchedNotes = new boolean[NUM_KEYS];
+		Iterator<Integer> iterator = touchPoints.keySet().iterator();
+		while (iterator.hasNext()) {
+			int key = iterator.next();
+
+			PointF point = touchPoints.get(key);
+			int note = getNoteForPixel(point.x, point.y);
+			if (note != -1) {
+				newTouchedNotes[note - KEY_NOTE_OFFSET] = true;
+			}
 		}
 		for (int i = 0; i < NUM_KEYS; i++) {
+			if (!touchedNotes[i] && newTouchedNotes[i]) {
+				shadeKey(i + KEY_NOTE_OFFSET, 1);
+			}
 			if (touchedNotes[i] && !newTouchedNotes[i]) {
-				// TODO
 				shadeKey(i + KEY_NOTE_OFFSET, 0);
 			}
 		}
 		touchedNotes = newTouchedNotes;
+
+		//		switch (event.getAction()) {
+		//		case MotionEvent.ACTION_DOWN:
+		//		case MotionEvent.ACTION_MOVE:
+		//		case MotionEvent.ACTION_POINTER_DOWN:
+		//			int touchedNote = getNoteForPixel(event.getX(), event.getY()) - KEY_NOTE_OFFSET;
+		//			if (!touchedNotes[touchedNote]) {
+		//				// TODO
+		//				shadeKey(touchedNote + KEY_NOTE_OFFSET, 1);
+		//			}
+		//			newTouchedNotes[touchedNote] = true;
+		//		}
+		//		for (int i = 0; i < NUM_KEYS; i++) {
+		//			if (touchedNotes[i] && !newTouchedNotes[i]) {
+		//				// TODO
+		//				shadeKey(i + KEY_NOTE_OFFSET, 0);
+		//			}
+		//		}
+		//		return true;
 		return true;
 	}
 }
